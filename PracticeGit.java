e96828b294bd1dc399c5d36c08938fc0d7192cc3
AllTheSquares <74791172+AllTheSquares@users.noreply.github.com>
2022-10-17 17:00:30 -0400

    Added practice for git.

---
+++ PracticeGit.java
@@ -0,0 +1,16 @@
+// This is a test program for us to learn how to use the repository. 
+// Goal: be able to push, pull, and commit changes to the GitHub repository.
+public class PracticeGit{
+    // Note this code is from W3Schools, in case you want to learn Java on your own time.
+    public static void main(String[] args) {
+        // TODO: add your own print statement following this syntax here.
+        // Follow appropriate Java syntax. The code here is exactly how it should be.
+        // Replace the message with your own and sign with your name at the end.
+
+        System.out.println("Hello world! -> Ryan");
+        
+        // Your messages:
+        
+
+    }
+}
