f806ec0cebf89f32339c172f18ea583baff0116b
AllTheSquares <74791172+AllTheSquares@users.noreply.github.com>
2023-01-19 16:10:43 -0500

    u

--- Robot_Project/2023_ChargedUp_RobotCode/2023_RobotCode/src/main/java/frc/robot/Robot.java
+++ Robot_Project/2023_ChargedUp_RobotCode/2023_RobotCode/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,6 +20,7 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -29,13 +31,13 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    gyro.calibrate();
   }
 
   /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
    * SmartDashboard integrated updating.
    */
   @Override
