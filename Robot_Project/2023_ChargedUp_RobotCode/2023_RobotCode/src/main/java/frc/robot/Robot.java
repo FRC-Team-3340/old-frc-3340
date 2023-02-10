bfb44d90b7c8417562f6fb18a08e8c5d316627df
AllTheSquares <74791172+AllTheSquares@users.noreply.github.com>
2023-02-10 00:56:04 -0500

    Added variables corresponding to controls.

--- Robot_Project/2023_ChargedUp_RobotCode/2023_RobotCode/src/main/java/frc/robot/Robot.java
+++ Robot_Project/2023_ChargedUp_RobotCode/2023_RobotCode/src/main/java/frc/robot/Robot.java
@@ -22,7 +22,11 @@ package frc.robot;
   // WPILib Other Libraries
   import edu.wpi.first.wpilibj.Joystick;                         // Flight stick interface to control the robot's parts
   import edu.wpi.first.wpilibj.drive.DifferentialDrive; // Tank drive - interfacing with the motors of the robot
-  import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.event.BooleanEvent;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+  import edu.wpi.first.networktables.DoublePublisher;
+  import edu.wpi.first.networktables.NetworkTable;
+  import edu.wpi.first.networktables.NetworkTableInstance; 
   //STOP: Imports for sensors, motors, and inputs
 //STOP: Import Statements
 
@@ -49,39 +53,54 @@ public class Robot extends TimedRobot {
       private String m_autoSelected;
       private final SendableChooser<String> m_chooser = new SendableChooser<>();  
   
+    // Fixed Numbers - please keep these to a minimum as you cannot edit these in code
+      // Motor Control - left/right motor IDs
+      private static final int motorID_LF = 1;       // Front Left Motor ID
+      private static final int motorID_RF = 2;       // Front Right Motor ID
+      private static final int motorID_LR = 3;       // Rear Left Motor ID
+      private static final int motorID_RR = 4;       // Rear Right Motor ID
+    
+    // Motors and Sensors - comment what each does
+      private AHRS navX_gyro = new AHRS(SPI.Port.kMXP); // initialize navX gyroscope class to interface with the gyro @ port SPI-MXP
+
+    // Motors - initialize individual motors to later group together
+      private CANSparkMax robot_motorLF = new CANSparkMax(motorID_LF, MotorType.kBrushless);  // create object for front left motor
+      private CANSparkMax robot_motorLR = new CANSparkMax(motorID_LR, MotorType.kBrushless);  // create object for rear left motor
+      private CANSparkMax robot_motorRF = new CANSparkMax(motorID_RF, MotorType.kBrushless);  // create object for front right motor
+      private CANSparkMax robot_motorRR = new CANSparkMax(motorID_RR, MotorType.kBrushless);  // create object for rear right motor
+
+    // Connect both motors together to act as one
+      private MotorControllerGroup trackL = new MotorControllerGroup(robot_motorLF, robot_motorLR); // create object for left track 
+      private MotorControllerGroup trackR = new MotorControllerGroup(robot_motorRF, robot_motorRR); // create object for right track
+
     // initialize robot and control system
-      private DifferentialDrive robot; // Create robot movement object
-      private Joystick robotMove_ControlStick; // Create joystick interface object
-      private Joystick robotArm_ControlStick; // Create another joystick for controlling the robot arm.
+      private DifferentialDrive robot = new DifferentialDrive(trackL, trackR); // Create robot movement object
+      private Joystick robot_joystick = new Joystick(0); // Create joystick interface object
     
     // Variables
-    private final double maximum_power = .40; // Base maximum power
-    private double DrivePower;  // Power management for robot
+      private double maximum_power = .40; // Base maximum power
+      private double DrivePower;  // Power management for robot
 
-    //START: Fixed Numbers - please keep these to a minimum as you cannot edit these in code
-      // PORT IDENTIFICATION
-        // Motor Control - left/right motor IDs
-        private static final int leftMotor_deviceID = 1;
-        private static final int rightMotor_deviceID = 2;
-        private static final int leftMotor2_deviceID = 3;
-        private static final int rightMotor2_deviceID = 4;
-
-
-
-    //STOP: Fixed Numbers
-
-    // Motors and Sensors - comment what each does
-        private AHRS navX_gyro; // initialize navX gyroscope class to interface with the gyro @ port SPI-MXP
-
-        // Motors
-        private MotorControllerGroup robot_leftMotor;
-        private CANSparkMax leftMotor_front;  // create object for left motor 
-        private CANSparkMax leftMotor_back; 
-        private MotorControllerGroup robot_rightMotor;
-        private CANSparkMax rightMotor_front; // create object for right motor
-        private CANSparkMax rightMotor_back; 
+      public NetworkTableInstance inst = NetworkTableInstance.getDefault();
+      public NetworkTable autobalance_table = inst.getTable("datatable");
+      public DoublePublisher autobalance_cast;
+      public double additive_power;
 
   // STOP: Initialize classes
+
+  // To make it easier for coding team, I listed most of the controls we will be using here.
+    double joystickY = robot_joystick.getY(); 
+    double joystickX = robot_joystick.getX();
+    double joystickZ = robot_joystick.getZ();
+    double joystickSlider = robot_joystick.getRawAxis(3);
+    BooleanEvent joystickTrigger = robot_joystick.button(1, null);
+    BooleanEvent DPad_Up = robot_joystick.povUp(null);
+    BooleanEvent DPad_Down = robot_joystick.povDown(null);
+
+    double gyroscope_roll = navX_gyro.getRoll();
+
+    
+    
 
   /**   
    * This function is run when the robot is first started up and should be used for any
@@ -90,32 +109,14 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    // Link motors, input devices, and sensors to variables.
-      leftMotor_front = new CANSparkMax(leftMotor_deviceID, MotorType.kBrushless);
-      leftMotor_back = new CANSparkMax(leftMotor2_deviceID, MotorType.kBrushless);
-      rightMotor_front = new CANSparkMax(rightMotor_deviceID, MotorType.kBrushless);
-      rightMotor_back = new CANSparkMax(rightMotor2_deviceID, MotorType.kBrushless);
-
-    // Connect both motors together to act as one
-      robot_leftMotor = new MotorControllerGroup(leftMotor_front, leftMotor_back);
-      robot_rightMotor = new MotorControllerGroup(rightMotor_front, rightMotor_back);
-
-      navX_gyro = new AHRS(SPI.Port.kMXP);
-
-    // Joysticks
-      robotMove_ControlStick = new Joystick(0);         // joystick that controls the robot
-      robotArm_ControlStick = new Joystick(1);      // joystick that controls the robot arm
-
-    // Initialize motors and sensors to neutral point to ensure no misreadings occur.
+    // Initialize motors and sensors to ensure no misreadings occur.
       navX_gyro.calibrate();
+      trackL.setInverted(true);
 
     // Initialize robot
-      robot_leftMotor.setInverted(true);
-      robot = new DifferentialDrive(robot_leftMotor, robot_rightMotor);
-      
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+      m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+      m_chooser.addOption("My Auto", kCustomAuto);
+      SmartDashboard.putData("Auto choices", m_chooser);
     }
 
   /**
@@ -127,7 +128,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
-
   }
 
   /**
@@ -180,11 +180,15 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    autobalance_cast= autobalance_table.getDoubleTopic("additive_power").publish();
+  }
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+    autobalance_robot();
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
@@ -193,24 +197,22 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {
-    /*
-    Adjust the Drive Power using the slider on the flight stick. Note that the axes of joysticks are always doubles.
-    */
-    DrivePower = maximum_power * (Math.abs((robotMove_ControlStick.getRawAxis(3) - 1)) / 2); 
-    drive(robotMove_ControlStick.getY(), robotMove_ControlStick.getZ(), DrivePower);
-    // System.out.println(robotMove_ControlStick.getRawButton(1));
-    // System.out.println(navX_gyro.getRoll());
-    // robotControl.tankDrive(robot_ControlStick.getY()*DrivePower, robot_ControlStick.getY()*DrivePower);
-    // System.out.println(navX_gyro.getPitch());
+  public void testPeriodic() { 
+    DrivePower = maximum_power * (Math.abs(joystickSlider - 1)) / 2;  /*  What does this do?
+     *    This allows the slider to control the speed of the robot.
+     *  Why do we find the absolute value of the slider minus 1 and then divide it by 2?
+     *    This normalizes the slider to a range of 0 to 1, as flippig it up makes it go to the negatives. */
 
+    drive(joystickY, joystickX, DrivePower);
 
  
   }
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+    // double tiltAxis = robot_joystick.getY() * 15;      // DEBUG: Emulate gyroscope using joystick
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
@@ -224,22 +226,19 @@ public class Robot extends TimedRobot {
   public void autobalance_robot() {
     float max_incline = 15;
     double autobalance_threshold = 2.5;
-    double additive_power;
-    // double tiltAxis = navX_gyro.getRoll();
-    double tiltAxis = robotMove_ControlStick.getY() * 15;
+    double tiltAxis = gyroscope_roll;
+  
     double max_additive_power = 0.1;
 
     if (tiltAxis > autobalance_threshold) {
       additive_power = (max_additive_power * ((tiltAxis - autobalance_threshold)/(max_incline - autobalance_threshold)));
-      if (additive_power < 0) {
-        additive_power = 0;
-      };
-    } else if (tiltAxis > -autobalance_threshold) {
+
+    } else if (tiltAxis < -autobalance_threshold) {
       additive_power = -(max_additive_power * ((tiltAxis + autobalance_threshold)/(-max_incline + autobalance_threshold))); 
-      if (additive_power > 0) {
-        additive_power = 0;
-      };   
+    } else {
+      additive_power = 0;
     };
-  };
-  
+
+    autobalance_cast.set(additive_power);
+  }; 
 }
