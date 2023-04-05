51a8beac56aa906419f689f0e8fecb2199e2b570
AllTheSquares <74791172+AllTheSquares@users.noreply.github.com>
2023-04-05 09:00:40 -0400

    Changes to navbar and carousel

--- src/components/showcase.tsx
+++ src/components/showcase.tsx
@@ -20,7 +20,7 @@ render()
                     />
                     <Carousel.Caption>
                         <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <p>This is where recent stuff would go.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -32,7 +32,7 @@ render()
 
                     <Carousel.Caption>
                         <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>Scrapbook thing.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -45,7 +45,7 @@ render()
                     <Carousel.Caption>
                         <h3>Third slide label</h3>
                         <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            Meet the team?
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
